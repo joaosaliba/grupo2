5868dc0e73cbe6cb5b376517d93eaad2ba02193c
Joao Gabriel Saliba Ribeiro <joao_salibaa@hotmail.com>
2024-04-16 20:41:41 -0300

    feat: user repository

---
+++ src/main/java/com/game/pokedex/repositories/UserRepository.java
@@ -0,0 +1,18 @@
+package com.game.pokedex.repositories;
+
+import com.game.pokedex.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    Boolean existsByEmail(String email);
+
+
+}
