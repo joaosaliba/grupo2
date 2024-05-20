d27fb2c9e024b913814f8a9538438a3400fef733
Francisco Gabriel <galvesa9@gmail.com>
2024-05-20 10:20:39 -0300

    criando UserControllerTest

---
+++ src/test/java/com/game/pokedex/controllers/UserControllerTest.java
@@ -0,0 +1,11 @@
+package com.game.pokedex.controllers;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserControllerTest {
+
+    private UserController userController;
+
+
+}
