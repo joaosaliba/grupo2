862bed2c8c4f4aabae016b68e6d5ddf22e093944
TiagoCSoares <tiagocsoares22@gmail.com>
2024-05-22 14:20:40 -0300

    Adicionando testes restantes de PokedexService

--- src/test/java/com/game/pokedex/service/PokedexServiceTest.java
+++ src/test/java/com/game/pokedex/service/PokedexServiceTest.java
@@ -1,10 +1,13 @@
 package com.game.pokedex.service;
 
 
+import com.game.pokedex.dtos.Evolution;
+import com.game.pokedex.dtos.PokemonSpecies;
 import com.game.pokedex.entities.Pokedex;
 import com.game.pokedex.entities.User;
 import com.game.pokedex.repositories.PokedexEndPointRepository;
 import com.game.pokedex.repositories.PokedexRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,12 +18,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -29,14 +33,6 @@ public class PokedexServiceTest {
     @Autowired
     private MockMvc mockMvc;
 
-    /*@Autowired
-    private PokedexRepository pokedexRepository;
-
-    @Autowired
-    private PokedexEndPointRepository pokedexEndPointRepository;
-
-    @Autowired
-    private RestTemplate restTemplate;*/
     @Mock
     private PokedexRepository pokedexRepository;
 
@@ -67,6 +63,7 @@ public class PokedexServiceTest {
         ).thenReturn(pokedex);
 
         Mockito.when(pokedexRepository.findByUserId(2L)).thenReturn(null);
+
     }
 
     @Test
@@ -82,5 +79,82 @@ public class PokedexServiceTest {
         var foundPokedex = pokedexService.listPokedexByUserId(2L);
 
         assertNull(foundPokedex);
+    }
+
+
+    @Test
+    public void deletePokedexByUserId_whenUserExists_returnIt() {
+
+        pokedexService.deletePokedexByUserId(user.getId());
+
+        Mockito.verify(pokedexRepository, times(1)).deletePokedexByUserId(user.getId());
+    }
+
+    @Test
+    public void deletePokedexByUserId_whenUserNotExists_EntityNotFoundException() {
+        doThrow(new EmptyResultDataAccessException(1)).when(pokedexRepository).deletePokedexByUserId(2L);
+
+        assertThrows(EntityNotFoundException.class, () -> {
+            pokedexService.deletePokedexByUserId(2L);
+        });
+
+       Mockito.verify(pokedexRepository, times(1)).deletePokedexByUserId(2L);
+    }
+
+    @Test
+    public void deletePokemonById_whenPokemonExists_returnIt() {
+        pokedexService.deletePokemonById(pokedex.getId());
+
+        Mockito.verify(pokedexRepository, times(1)).deleteById(pokedex.getId());
+    }
+
+    @Test
+    public void deletePokemonById_whenPokemonNotExists_EntityNotFoundException() {
+        doThrow(new EmptyResultDataAccessException(1)).when(pokedexRepository).deleteById(2L);
+
+        assertThrows(EntityNotFoundException.class, () -> {
+            pokedexService.deletePokemonById(2L);
+        });
+
+        Mockito.verify(pokedexRepository, times(1)).deleteById(2L);
+    }
+
+
+    @Test
+    public void addPokemonToPokedex_whenPokemonExists_returnIt() {
+        pokedexService.addPokemonToPokedex(pokedex);
+
+        Mockito.verify(pokedexRepository, times(1)).save(pokedex);
+    }
+
+    @Test
+    public void savePokedexEntry_whenPokemonExists_returnIt() {
+        pokedexService.savePokedexEntry(pokedex);
+
+        Mockito.verify(pokedexRepository, times(1)).save(pokedex);
+    }
+
+    @Test
+    public void evolvePokemon_whenPokemonExists_returnIt() {
+        //PokemonSpecies
+        Evolution evolution = new Evolution("url");
+        Mockito.when(
+                pokedexEndPointRepository.getByEvolutionChain("pokemon_name")
+        ).thenReturn(new PokemonSpecies(evolution));
+
+        var pokemonSpecies = pokedexService.evolvePokemon("username", "pokemon_name");
+
+        assertNotNull(pokemonSpecies);
+    }
+
+    @Test
+    public void evolvePokemon_whenPokemonNotExists_returnNull() {
+        Mockito.when(
+                pokedexEndPointRepository.getByEvolutionChain("pokemon_name")
+        ).thenReturn(null);
+
+        var pokemonSpecies = pokedexService.evolvePokemon("username", "pokemon_name");
+
+        assertNull(pokemonSpecies);
     }
 }
